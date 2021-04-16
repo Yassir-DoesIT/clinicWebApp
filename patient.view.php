c08066dff770a7dd46c7e86e02ba1417f25e5bc4
Yasser Bekhti <yasser.bekhti.15@gmail.com>
2021-04-16 02:54:56 +0000

    made a profile page

--- patient.view.php
+++ patient.view.php
@@ -5,5 +5,97 @@ if (isset($_SESSION['prenom']) && isset($_SESSION['nom'])) {
 	echo "Hello ". $_SESSION['prenom'] . " " . $_SESSION['nom'] . ", your id is " . $_SESSION['user_id'];}
 ?>
 <div class="w3-display-container"><button class="w3-display-topright"><a href="logout">Log Out</a></button>
+<div class="w3-sidebar w3-pale-blue w3-bar-block w3-card w3-animate-left" style="display:none" id="mySidebar">
+                <button class="w3-bar-item w3-button w3-large"
+                onclick="w3_close()">Close &times;</button>
+                <form action="/action_page.php">
+                    <input class="w3-input" type="text" placeholder="Search.." name="search">
+                    <button class="w3-button" type="submit"><i class="fa fa-search"></i></button>
+                  </form>
+                <a href="#" class="w3-bar-item w3-button">Mes Informations</a>
+                <a href="#" class="w3-bar-item w3-button">Mes Consultations</a>
+                <a href="#" class="w3-bar-item w3-button">Boite de Réception</a>
+              </div>
+
+     <div class="w3-display-container w3-teal">
+
+                <div class="w3-container w3-padding-64">
+
+                    <img src="Images/Logo.png" class="w3-display-left" style="max-height: 150px; max-width: 150px"></img>
+
+                  </div>
+        </div>
+
+    <div class="w3-bar w3-teal w3-right-align">
+
+            <button id="openNav" style="float: left" class="w3-button w3-teal w3-xlarge" onclick="w3_open()">&#9776;</button>
+            <button class="w3-button w3-teal w3-hover-gray" onclick="goToEtab()">Consulter Les Etablissments de Santé</button>
+            <button class="w3-button w3-teal w3-hover-gray" onclick="goToLogOut()">Se Déconnecter</button>
+            
+
+    </div>
+            
+
+
+    <div id="mainDiv">
+
+            <div class="w3-row-padding w3-center w3-section">
+
+                    <div class="w3-card w3-container w3-teal w3-center w3-round-large" id="#home">
+                               
+                               <div class="w3-section">
+           
+                                       <div class="w3-container w3-margin w3-padding-large w3-round-large">
+           
+                                           <h3>Mes Informations</h3>
+                                           <div class="w3-card w3-margin w3-round-large w3-border-red w3-pale-blue" >
+           
+                                               <div class="w3-display-container" style="font-size: 20px; font-weight: 200; font-family: 'Open Sans Condensed';">
+                                                   <img src="placeHolder.jpg" class="w3-circle w3-display-topleft" alt="profile_picture" style="width: 80px; height: 80px;margin-left: 20px; margin-top: 10px"> 
+                                                   <h3 style="padding-top: 15px">USERNAME</h3>
+                                                   <span>EMAIL</span>
+                                                   <table class="w3-table w3-margin-top" style="margin-left: 100px">
+                                                       <form action="placeHolder.php">
+                                                           <tr>
+                                                               <td ><b>Prénom</b></td>
+                                                               <td><input style="width: 70%" type="text" name="first_name" class="w3-input w3-border"  readonly></td>
+                                                           </tr>
+                                                           <tr>
+                                                               <td><b>Nom</b></td>
+                                                               <td><input style="width: 70%" type="text" name="last_name" class="w3-input w3-border"  readonly></td>
+                                                           </tr>
+                                                           <tr>
+                                                               <td><b>Email</b></td>
+                                                               <td><input style="width: 70%" type="text" name="mail" class="w3-input w3-border" readonly ></b></td>
+                                                           </tr>
+                                                           <tr>
+                                                               <td><b>Date de Naissance</b></td>
+                                                               <td><input style="width: 70%" type="text" name="date" class="w3-input w3-border" readonly></td>
+                                                           </tr>
+                                                           <tr>
+                                                                   <td><b>Sexe</b></td>
+                                                                   <td><input class="w3-radio" type="radio" name="gender" value="male" checked>
+                                                                       <label>Male</label>
+                                                                   <input class="w3-radio" type="radio" name="gender" value="female" style="margin-left: 50px">
+                                                                       <label>Female</label>
+                                                                   </td>
+                                                           </tr>
+                                                           <tr>
+           
+                                                                   <td><b>Mot de Passe</b></td>
+                                                                   <td><input style="width: 70%" type="password" name="password" class="w3-input w3-border" readonly></td>
+           
+                                                           </tr>
+                                                       </form>
+                                                   </table>
+                                                   
+                                               </div>
+           
+                                               
+                               </div>
+           
+               </div>
+
+    </div>
 
 <?php require('Partials/footer.php')?>
--- someFrontEnd.js
+++ someFrontEnd.js
@@ -33,6 +33,18 @@ function show(cityName)
         xhr.send();
     }
 
+    function w3_open() {
+        document.getElementById("mainDiv").style.marginLeft = "25%";
+        document.getElementById("mySidebar").style.width = "25%";
+        document.getElementById("mySidebar").style.display = "block";
+        document.getElementById("openNav").style.display = 'none';
+      }
+      function w3_close() {
+        document.getElementById("mainDiv").style.marginLeft = "0%";
+        document.getElementById("mySidebar").style.display = "none";
+        document.getElementById("openNav").style.display = "inline-block";
+      }
+
 function buildMap(latVar,lngVar,quartierId)
     {
 
