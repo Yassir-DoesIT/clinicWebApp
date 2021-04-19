e92a42cbf4f1b954409bba838c312311c72359b1
Yasser Bekhti <yasser.bekhti.15@gmail.com>
2021-04-19 17:08:37 +0000

    admin login page draft and added button that I forgot

--- profile.view.php
+++ profile.view.php
@@ -128,6 +128,7 @@ onclick="w3_close()">Close &times;</button>
         
               <div id="buttonsDiv" class="w3-container w3-border-top w3-padding-16">
                 <button id="closeButton" onclick="closeSendModal()" type="button" class="w3-button w3-red">Fermez</button>
+                <button id='sendButton' type='submit' type='button' class='w3-button w3-green'>Envoyez</button>
               </div>
         </form>
         
